769e6a391c56f1617bfbb580d037e814445ab0e1
sergeythrees <sergeythrees@gmail.com>
2019-01-17 14:22:02 +0300

    fix lab4

--- haskell/lab4.hs
+++ haskell/lab4.hs
@@ -1,4 +1,6 @@
-module LW4 (mySplitAt, mySize, myIntersperse, myIntersection) where
+module LAB4 (mySplitAt, mySize, myMember, myFromList, myIntersperse, myIntersection) where
+import System.IO
+import Data.Map
 
 mySplitAt 0 xs     = ([], xs)
 mySplitAt n []     = ([], [])
@@ -6,8 +8,16 @@ mySplitAt n (x:xs) =  (x:ys, zs)
 	where (ys, zs) = mySplitAt (n - 1) xs
 
 
-mySize :: M.Map k a -> Int
-mySize arg = length (M.keys arg)
+mySize :: Data.Map.Map k a -> Int
+mySize arg = length (Data.Map.keys arg)
+
+myMember :: (Eq a) => a -> [a] -> Bool
+myMember x [] = False
+myMember x (y:ys) = if (y == x) then True else myMember x ys
+
+myFromList :: (Eq a) => [a] -> [a]
+myFromList [] = []
+myFromList (x:xs) = if myMember x (myFromList xs) then myFromList xs else x:(myFromList xs)
 
 
 myIntersperse x (y:ys) = y:concat[[x,z] | z <- ys]
